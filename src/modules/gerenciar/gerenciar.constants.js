5482f96145ecfde36384d5acf304566b16c24d3e
Felipe Ribeiro Machado <feliperibeiromachado@edu.univali.br>
2022-08-24 17:17:21 -0300

    correção de bugs

--- src/modules/gerenciar/gerenciar.constants.js
+++ src/modules/gerenciar/gerenciar.constants.js
@@ -1,15 +1,15 @@
 export const GERENCIAR_URL = Object.freeze({
   path: '/gerenciar',
-  livros: {
-    name: 'gerenciar.livros',
-    path: '/livros',
+  administrador: {
+    name: 'gerenciar.administrador',
+    path: '/administrador',
   },
-  emprestimos: {
-    name: 'gerenciar.emprestimos',
-    path: '/emprestimos',
+  imovel: {
+    name: 'gerenciar.imovel',
+    path: '/imovel',
   },
-  usuarios: {
-    name: 'gerenciar.usuarios',
-    path: '/usuarios',
+  tipoImovel: {
+    name: 'gerenciar.tipoImovel',
+    path: '/tipoImovel',
   },
 });
